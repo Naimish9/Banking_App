ff6b4af3fcd2ce91a6fd5d475c4de03224ac6fb9
Naimish9 <naimish.bareek@nineleaps.com>
2024-05-03 18:20:30 +0530

    updated registration function03

--- App_Prog/options.py
+++ App_Prog/options.py
@@ -80,38 +80,42 @@ def initiate_deposit(cursor, connect, user):
 #3. Add Beneficiaries
 def add_beneficiary(cursor, connect, user):
     print("Add Beneficiary Details:")
-    while True:
-        #username = user[1]
-        benef_name = input("Enter beneficiary name: ")
-        benef_account_number = int(input("Enter beneficiary account number: "))
-        benef_ifsc = input("Enter beneficiary IFSC Code: ")
+    try:
+        while True:
+            #username = user[1]
+            benef_name = input("Enter beneficiary name: ")
+            benef_account_number = int(input("Enter beneficiary account number: "))
+            benef_ifsc = input("Enter beneficiary IFSC Code: ")
+    
+            cursor.execute("SELECT * FROM acc_info WHERE acc_no = %s AND user_name = %s",
+                           (benef_account_number, benef_name))
+            beneficiary_data = cursor.fetchone()
+            if beneficiary_data is None:
+                print("Beneficiary account number does not match the provided name.")
+                retry = input("Do you want to retry? (yes/no): ").lower()
+                if retry != 'yes':
+                    break
+                continue
 
-        cursor.execute("SELECT * FROM acc_info WHERE acc_no = %s AND user_name = %s",
-                       (benef_account_number, benef_name))
-        beneficiary_data = cursor.fetchone()
-        if beneficiary_data is None:
-            print("Beneficiary account number does not match the provided name.")
-            retry = input("Do you want to retry? (yes/no): ").lower()
-            if retry != 'yes':
-                break
-            continue
+            # Check if the benef_ifsc exists in the Benf table
+            cursor.execute("SELECT * FROM BankIFSC WHERE IFSC_Code = %s", (benef_ifsc,))
+            if not cursor.fetchone():
+                print("Beneficiary IFSC Code does not exist in the database.")
+                retry = input("Do you want to retry? (yes/no): ").lower()
+                if retry != 'yes':
+                    break
+                continue
 
-        # Check if the benef_ifsc exists in the Benf table
-        cursor.execute("SELECT * FROM BankIFSC WHERE IFSC_Code = %s", (benef_ifsc,))
-        if not cursor.fetchone():
-            print("Beneficiary IFSC Code does not exist in the database.")
-            retry = input("Do you want to retry? (yes/no): ").lower()
-            if retry != 'yes':
-                break
-            continue
-
-        # Insert the beneficiary into the beneficiaries table
-        sql = "INSERT INTO Benf (user_name, benf_name, benf_acc_no, Benf_ifsc) VALUES (%s, %s, %s, %s)"
-        val = (user[1], benef_name, benef_account_number, benef_ifsc)
-        cursor.execute(sql, val)
-        connect.commit()
-        print("Beneficiary added successfully!")
-        break
+            # Insert the beneficiary into the beneficiaries table
+            sql = "INSERT INTO Benf (user_name, benf_name, benf_acc_no, Benf_ifsc) VALUES (%s, %s, %s, %s)"
+            val = (user[1], benef_name, benef_account_number, benef_ifsc)
+            cursor.execute(sql, val)
+            connect.commit()
+            print("Beneficiary added successfully!")
+            break
+    except Exception as e:
+        print("An error occurred, enter details again: ", e)
+        add_beneficiary(cursor, connect, user)
 
     list_beneficiaries(cursor, connect, user)
 
